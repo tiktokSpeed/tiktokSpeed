486eaa5d02e36f571e869b50d21cbee38a02c21d
MinyiZou <suusatoshigi@icloud.com>
2023-08-20 17:06:08 -0700

    feat: add user login feature

--- cmd/user/dao/user.go
+++ cmd/user/dao/user.go
@@ -41,3 +41,12 @@ func NewUser(username string, password string) (*base.User, error) {
 		Name: username,
 	}, nil
 }
+
+func GetUserByUsername(username string) (*model.User, error) {
+	user := &model.User{}
+	err := initialize.DB.Table("user").Where(&model.User{Username: username}).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
